307a546d8740ccf02fd939ba6871ef3a902de96e
弓耀玲 <zhangll19900904@163.com>
2019-02-24 17:24:00 +0800

    file handle

--- python/learn-python-file.py
+++ python/learn-python-file.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import linecache
 
 count = 0
@@ -14,4 +15,17 @@ thefile.close( )
 
 print count
 
-count = linecache.getline('nginx.log',linenum)
+fo = open('nginx.log', 'rb')
+line = fo.readline()
+print line
+fo.seek(2, 0) #offset -- 开始的偏移量，也就是代表需要移动偏移的字节数 第二个参数意义: 0代表从文件开头开始算起，1代表从当前位置开始算起，2代表从文件末尾算起
+line = fo.readline()
+print line
+
+count = linecache.getline('nginx.log',3)
+print count
+print count.find("dfa")
+print count.find("fgdc")
+
+counts = linecache.getlines('nginx.log',3)
+print counts
